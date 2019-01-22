b27876d32195e65412e3f76c5b558a7b8ddbe6e5
wonkatin <j.drawers@gmail.com>
2019-01-21 22:46:58 -0500

    Fixed Errors (11th Commit)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar'
 import BusinessList from './components/BusinessList/BusinessList'
 
-class App extends react.Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
