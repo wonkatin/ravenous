47972b75d29754d9999a973269bf71d338d2e3e3
wonkatin <j.drawers@gmail.com>
2019-01-30 11:55:34 -0500

    Import Yelp to App JS and removed hard coded business and businesses (16th Commit)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar/SearchBar'
-import BusinessList from './components/BusinessList/BusinessList'
-
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [
-  business, business, business, business, business, business,
-];
+import SearchBar from './components/SearchBar/SearchBar';
+import BusinessList from './components/BusinessList/BusinessList';
+import Yelp from '././util/Yelp';
 
 class App extends React.Component {
   searchYelp(term, location, sortBy){
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -29,4 +29,4 @@ const Yelp = {
   }
 };
 
-export default yelp;
+export default Yelp;
